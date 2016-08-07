193b451950c6550dee24a509c3abe11761fd564a
zk516 <yangpukun@gmail.com>
2016-08-07 23:13:31 +1200

    finished junit test

--- src/cluedo/game/CluedoTest.java
+++ src/cluedo/game/CluedoTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import cluedo.cards.Card;
 import cluedo.cards.Characters;
+import cluedo.cards.Places;
 
 public class CluedoTest {
 
@@ -94,6 +95,7 @@ public class CluedoTest {
 	}
 
 	@Test
+	//player can not move outside the board
 	public void test07_checkBoardBoundry(){
 		try{
 			Game game = setUpMockGame();
@@ -104,13 +106,217 @@ public class CluedoTest {
 		}
 	}
 
-//	@Test
-//	public void test08_checkInput(){
-//		try{
-//			Game game = setUpMockGame();
-//			game.
-//		}
-//	}
+	@Test
+	//check move south option
+	public void test08_ValidMove01(){
+		try{
+			Game game = setUpMockGame();
+			int i = game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			assert game.playTeam.get(0).getP().equals(new Position(1,9));
+		}catch(IOException e){
+
+		}
+	}
+
+	@Test
+	//when player uses "move west", the remaining steps should change
+	public void test09_ValidMove02(){
+		try{
+			Game game = setUpMockGame();
+			assert game.executeTheChoice("move west",game.playTeam.get(0), game.board, game)==0;
+
+		}catch(IOException e){
+
+		}
+	}
+
+	@Test
+	//when player uses "check cards", the remaining steps should not change
+	public void test10_ValidMove03(){
+		try{
+			Game game = setUpMockGame();
+			assert game.executeTheChoice("check cards", game.playTeam.get(0), game.board, game)==2;
+
+		}catch(IOException e){
+
+		}
+	}
+
+	@Test
+	//when player uses "end turn", the remaining steps should not change
+	public void test11_ValidMove04(){
+		try{
+			Game game = setUpMockGame();
+			assert game.executeTheChoice("end turn", game.playTeam.get(0), game.board, game)==1;
+
+		}catch(IOException e){
+
+		}
+	}
+
+	@Test
+	//player can enter a room
+	public void test12_ValidMove_05(){
+		try{
+			Game game = setUpMockGame();
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("entry BallRoom",game.playTeam.get(0), game.board, game);
+		}catch(IOException e){
+
+		}
+	}
+
+	@Test
+	//check if the game can remove players
+	public void test13_removePlayer(){
+		try{
+			Game game = setUpMockGame();
+			game.lostTheGame(game.playTeam.get(0));
+			game.lostTheGame(game.playTeam.get(1));
+			game.lostTheGame(game.playTeam.get(2));
+			game.lostTheGame(game.playTeam.get(3));
+			assert game.playTeam.size()==2;
+		}catch(IOException e){
+
+		}
+	}
+//
+	@Test
+	//player can not make suggestion when is not in a room
+	public void test14_InValidMove(){
+		try{
+			Game game = setUpMockGame();
+			assertFalse( game.playTeam.get(0).isSuggestion());
+		}catch(IOException e){
+
+		}
+	}
+
+	@Test
+	//the player can know another card when entering a room
+	public void test15_checkKnowingCards(){
+		try{
+			Game game = setUpMockGame();
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			int i = game.playTeam.get(0).getKnowingCards().size();
+			game.executeTheChoice("entry BallRoom",game.playTeam.get(0), game.board, game);
+			assert i<game.playTeam.get(0).getKnowingCards().size();
+		}catch(IOException e){
+
+		}
+	}
+	@Test
+	//player can enter a room in the knockingDoor position
+	public void test16_checkEntryRoom(){
+		try{
+			Game game = setUpMockGame();
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			assert game.checkRoomOption(game.playTeam.get(0), game.board).equals("entry BallRoom");
+
+		}catch(IOException e){
+
+		}
+	}
+
+
+	@Test
+	//player can not use stair if there is no stair in a room
+	public void test17_checkStairs(){
+		try{
+			Game game = setUpMockGame();
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("entry BallRoom",game.playTeam.get(0), game.board, game);
+			assert game.checkStairOption(game.playTeam.get(0), game.board).equals(null);
+		}catch(IOException e){
+
+		}
+	}
+
+
+	@Test
+	//player can make suggestion in a room
+	public void test18_checkSuggestOption(){
+		try{
+			Game game = setUpMockGame();
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("entry BallRoom",game.playTeam.get(0), game.board, game);
+			assert game.checkSuggestionOption(game.playTeam.get(0), game.board).equals("make suggestion");
+		}catch(IOException e){
+
+		}
+	}
+
+	@Test
+	//player can exit room in a room
+	public void test19_checkExitRoomOption(){
+		try{
+			Game game = setUpMockGame();
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("entry BallRoom",game.playTeam.get(0), game.board, game);
+			assert game.checkExitRoomOption(game.playTeam.get(0), game.board).equals("exit room");
+		}catch(IOException e){
+
+		}
+	}
+	@Test
+	//check findPlayerLocation method
+	public void test20_checkFindPlayerLocation(){
+		try{
+			Game game = setUpMockGame();
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move west",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("move south",game.playTeam.get(0), game.board, game);
+			game.executeTheChoice("entry BallRoom",game.playTeam.get(0), game.board, game);
+			assert game.findPlayerLocation(game.playTeam.get(0), game.board).equals(Places.BALL_ROOM);
+		}catch(IOException e){
+
+		}
+	}
+
+
+
+
+
 
 	public Game setUpMockGame() throws IOException{
 		Board board = new Board("map.txt");
